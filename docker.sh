7c0f391981fc37c4bb6aff2776e9a4801a270c98
Alex Casse <acasse@hurondigitalpathology.com>
2022-10-24 11:29:47 -0400

    added bioformats java library

--- docker.sh
+++ docker.sh
@@ -6,7 +6,7 @@ echo "************************************** Publish docker ********************
 docker login git.hurondigitalpathology.com:5005
 
 #DOCKER_REPO="git.hurondigitalpathology.com:5005/pine/cytomine-ims"
-DOCKER_REPO="huronalex/ims"
+DOCKER_REPO="huronfrancois/ims"
 VERSION_NUMBER="2.3.0"
 
 DOCKER_BUILDKIT=1 docker build --rm -f docker/Dockerfile -t  $DOCKER_REPO:v$VERSION_NUMBER .
