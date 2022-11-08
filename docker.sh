bea43b824a20dab9dde68f92e8413f9d8c9520c2
Thomas Alex Casse <acasse@hurondigitalpathology.com>
2022-11-08 17:12:31 -0500

    Removed package

--- docker.sh
+++ docker.sh
@@ -6,8 +6,8 @@ echo "************************************** Publish docker ********************
 docker login git.hurondigitalpathology.com:5005
 
 #DOCKER_REPO="git.hurondigitalpathology.com:5005/pine/cytomine-ims"
-DOCKER_REPO="huronfrancois/ims"
-VERSION_NUMBER="2.3.0"
+DOCKER_REPO="huronalex/ims"
+VERSION_NUMBER="2.3.1"
 
 DOCKER_BUILDKIT=1 docker build --rm -f docker/Dockerfile -t  $DOCKER_REPO:v$VERSION_NUMBER .
 
